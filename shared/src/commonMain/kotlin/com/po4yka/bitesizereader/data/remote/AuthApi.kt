ae6cad8d1e0d55934d0e4e6426126e031409aaf3
Nikita Pochaev <pochaev.nik@gmail.com>
2025-12-07 16:35:32 +0400

    feat: Implement secret key authentication for user login and add related API endpoints.

--- shared/src/commonMain/kotlin/com/po4yka/bitesizereader/data/remote/AuthApi.kt
+++ shared/src/commonMain/kotlin/com/po4yka/bitesizereader/data/remote/AuthApi.kt
@@ -2,6 +2,7 @@ package com.po4yka.bitesizereader.data.remote
 
 import com.po4yka.bitesizereader.data.remote.dto.ApiResponseDto
 import com.po4yka.bitesizereader.data.remote.dto.AuthResponseDto
+import com.po4yka.bitesizereader.data.remote.dto.SecretLoginRequestDto
 import com.po4yka.bitesizereader.data.remote.dto.TelegramLoginRequestDto
 import com.po4yka.bitesizereader.data.remote.dto.TokenRefreshRequestDto
 import com.po4yka.bitesizereader.data.remote.dto.TokenRefreshResponseDto
@@ -9,6 +10,7 @@ import com.po4yka.bitesizereader.data.remote.dto.UserDto
 
 interface AuthApi {
     suspend fun loginWithTelegram(request: TelegramLoginRequestDto): ApiResponseDto<AuthResponseDto>
+    suspend fun secretLogin(request: SecretLoginRequestDto): ApiResponseDto<AuthResponseDto>
     suspend fun refreshToken(request: TokenRefreshRequestDto): ApiResponseDto<TokenRefreshResponseDto>
     suspend fun getCurrentUser(): ApiResponseDto<UserDto>
 }
