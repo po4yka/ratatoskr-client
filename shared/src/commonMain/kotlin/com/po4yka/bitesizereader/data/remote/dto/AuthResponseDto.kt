99cef2378fe52b380a5320ee313e80d274aafad0
Nikita Pochaev <pochaev.nik@gmail.com>
2025-12-07 21:27:25 +0400

    refactor: restructure AuthResponseDto to nest token details within a new TokensDto and update AuthMapper.

--- shared/src/commonMain/kotlin/com/po4yka/bitesizereader/data/remote/dto/AuthResponseDto.kt
+++ shared/src/commonMain/kotlin/com/po4yka/bitesizereader/data/remote/dto/AuthResponseDto.kt
@@ -4,7 +4,13 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+
 data class AuthResponseDto(
+    @SerialName("tokens") val tokens: TokensDto
+)
+
+@Serializable
+data class TokensDto(
     @SerialName("access_token") val accessToken: String,
     @SerialName("refresh_token") val refreshToken: String,
     @SerialName("token_type") val tokenType: String,
